9a8707a89d0e6d5a01854648e9c9fd8b68624295
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2024-10-16 03:14:28 +0600

    Fetching data from api and showing as card in ui

---
+++ script/custom.js
@@ -0,0 +1,37 @@
+async function getData() {
+    try {
+        const response = await fetch('https://gutendex.com/books/');
+        const data = await response.json();
+        const result = data?.results; // Process the data here
+        let card = '';
+        
+        // Loop through each book and generate HTML
+        result.forEach(element => {
+            card += `
+        <div class="books-card">
+            <img src="${element?.formats['image/jpeg']}" class="book-image" alt="book-cover-image">
+            <div class="book-info">
+                <div class="title-id">
+                    <h1 class="book-title">${element?.title}</h1>
+                </div>
+                <p class="book-author">${element?.authors[0]?.name}</p>
+                <p class="author-date">(${element?.authors[0]?.birth_year} - ${element?.authors[0]?.death_year})</p>
+                <p class="book-genre">Genre - ${element?.subjects[0]}</p>
+                <p class="book-id">ID: ${element?.id}</p>
+                <div class="btn-group">
+                    <a href="details.html?id=${element?.id}"><button class="view-btn">Book details</button></a>
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+            </div>
+        </div>`;
+        });
+        
+        // Set the generated HTML to the book list container
+        document.getElementsByClassName("book-list-container")[0].innerHTML = card;
+        console.log(card)
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+getData();
